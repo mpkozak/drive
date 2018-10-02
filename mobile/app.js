13f8bbd98dd22417416d4fc1d50eff16316cb69b
mpkozak <mparkerkozak@gmail.com>
2018-10-02 13:27:08 -0400

    more

--- mobile/app.js
+++ mobile/app.js
@@ -1,45 +1,4 @@
 
-// swipe stack
-
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
-
-var gesuredZone = document.getElementById('gamebox-parent');
-
-gesuredZone.addEventListener('touchstart', function(event) {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
-}, false);
-
-gesuredZone.addEventListener('touchend', function(event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesure();
-}, false);
-
-function handleGesure() {
-    var swiped = 'swiped: ';
-    if (touchendX < touchstartX) {
-        alert(swiped + 'left!');
-        moveLeft();
-    }
-    if (touchendX > touchstartX) {
-        alert(swiped + 'right!');
-        moveRight();
-    }
-    if (touchendY < touchstartY) {
-        alert(swiped + 'down!');
-        jump();
-    }
-    if (touchendY > touchstartY) {
-        alert(swiped + 'left!');
-    }
-    // if (touchendY == touchstartY) {
-    //     alert('tap!');
-    // }
-}
 
 
 
@@ -862,20 +821,19 @@ function handleGesure() {
       initializePlayerCar();
       titleFlyOut();
       playButtonExplode();
-      // tutorial();
-      initializeGamePlay();
+      tutorial();
     };
 
 // Function Tutorial Master Stack
     function tutorial() {
-      // makeTutorial();
-      // setTimeout(() => { tutorialAppear() }, 200);
-      // document.addEventListener('keydown', initializeGamePlay);
+      makeTutorial();
+      setTimeout(() => { tutorialAppear() }, 200);
+      document.addEventListener('click', initializeGamePlay);
     };
 
 // Function Initialize Gameplay Master Stack
     function initializeGamePlay() {
-      document.querySelector('#music').play();                //music play
+      // document.querySelector('#music').play();                //music play
       resetClock();
       resetDistance();
       speed = startSpeed;
@@ -980,3 +938,50 @@ function handleGesure() {
 
 // rAF Initialize
     window.requestAnimationFrame(drawGame);
+
+
+
+// swipe stack
+
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const gestureZone = document.querySelector('main');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+function handleGesture() {
+    if (touchendX <= touchstartX) {
+        console.log('Swiped left');
+        moveLeft();
+    }
+
+    if (touchendX >= touchstartX) {
+        console.log('Swiped right');
+        moveRight();
+    }
+
+    if (touchendY <= touchstartY) {
+        console.log('Swiped up');
+        jump();
+    }
+
+    // if (touchendY >= touchstartY) {
+    //     console.log('Swiped down');
+    // }
+
+    // if (touchendY === touchstartY) {
+    //     console.log('Tap');
+    // }
+}
