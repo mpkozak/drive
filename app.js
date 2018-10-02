aa4cef4afca72a91986ab49572db58a353c269be
mpkozak <mparkerkozak@gmail.com>
2018-10-02 19:47:04 -0400

    fixed mobile tutorial click event listener

--- app.js
+++ app.js
@@ -401,7 +401,8 @@
       tutorial.style.opacity = 0;
       tutorial.remove();
       p2.appendChild(tutorial);
-      setTimeout(() => { document.addEventListener('click', initializeGamePlay) }, 100);
+      // setTimeout(() => { document.addEventListener('click', initializeGamePlay) }, 100);
+      document.addEventListener('click', initializeGamePlay);
     };
 
 // Function Select Tutorial
